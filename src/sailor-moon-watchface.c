e29b9afa99d85c7a8dd815bb23de5effa35b273a
conscou <scoullisconstantina@gmail.com>
2016-10-04 00:21:45 -0400

    changed image sizes and positionings

--- src/sailor-moon-watchface.c
+++ src/sailor-moon-watchface.c
@@ -40,18 +40,18 @@ static void update_time() {
 
 static void update_sailorscout_symbol(struct tm *current_time){
 
-	time_layer = text_layer_create(GRect(0, 140, 144, 50));
+	time_layer = text_layer_create(GRect(0, 120, 144, 50));
     text_layer_set_background_color(time_layer, GColorBlack);
     text_layer_set_text_color(time_layer, GColorWhite);
     text_layer_set_text(time_layer, "00:00");
     
-    GFont custom_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_CRAFTY_GIRLS_20));
+    GFont custom_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_CRAFTY_GIRLS_24));
     text_layer_set_font(time_layer, custom_font);
     text_layer_set_text_alignment(time_layer, GTextAlignmentCenter);
     layer_add_child(window_get_root_layer(window), text_layer_get_layer(time_layer));
     
     
-    symbol_layer = bitmap_layer_create(GRect(0, 25, 144, 70));
+    symbol_layer = bitmap_layer_create(GRect(0, 25, 144, 90));
     bitmap_layer_set_background_color(symbol_layer, GColorBlack);
 
 
@@ -73,6 +73,7 @@ static void update_sailorscout_symbol(struct tm *current_time){
 			symbol_bitmap = gbitmap_create_with_resource(RESOURCE_ID_SATURN);		
 		}
 	
+	symbol_bitmap = gbitmap_create_with_resource(RESOURCE_ID_SATURN);	
 	
     bitmap_layer_set_bitmap(symbol_layer, symbol_bitmap);
     layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(symbol_layer));
